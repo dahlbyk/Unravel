c8746f0f04cf1c216009c6fcbb42ddafbdbe680d
Keith Dahlby <dahlbyk@gmail.com>
2021-09-18 11:18:23 -0500

    Check environment from HttpContext in ASP.NET MVC

--- examples/Web/Controllers/HomeController.cs
+++ examples/Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using Unravel;
 using UnravelExamples.Web.Services;
 
@@ -16,6 +17,25 @@ namespace UnravelExamples.Web.Controllers
             var services = Application.Services;
             var env = new EnvironmentCheck("Unravel.Application.Services", services);
             return Content(env.ToString(), "application/json; charset=utf-8");
+        }
+
+        public ActionResult HttpContextBase_GetRequestServices()
+        {
+            var services = HttpContext.GetRequestServices();
+            var env = new EnvironmentCheck("HttpContextBase.GetRequestServices()", services);
+            return Content(env.ToString(), "text/plain; charset=utf-8");
+        }
+
+        public ActionResult HttpContext_Current_GetRequestServices()
+        {
+            var services = System.Web.HttpContext.Current.GetRequestServices();
+            var env = new EnvironmentCheck("HttpContext.GetRequestServices()", services);
+            return Content(env.ToString(), "text/plain; charset=utf-8");
+        }
+
+        public ActionResult Throw()
+        {
+            throw new System.ApplicationException("From ASP.NET MVC");
         }
     }
 }
