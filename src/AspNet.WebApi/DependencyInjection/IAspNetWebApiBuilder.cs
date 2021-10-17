a03f84649588635307a61e4f057b53b4f3f46cfe
Keith Dahlby <dahlbyk@gmail.com>
2021-10-16 23:43:16 -0500

    Add IServiceCollection.AddAspNetWebApi()

---
+++ src/AspNet.WebApi/DependencyInjection/IAspNetWebApiBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    ///   An interface for configuring ASP.NET Web API services.
+    /// </summary>
+    public interface IAspNetWebApiBuilder
+    {
+        /// <summary>
+        ///   Gets the <see cref="IServiceCollection"/> where ASP.NET Web API services are configured.
+        /// </summary>
+        IServiceCollection Services { get; }
+    }
+}
---
+++ src/AspNet.WebApi/DependencyInjection/Internal/AspNetWebApiBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Unravel.AspNet.WebApi.DependencyInjection.Internal
+{
+    /// <summary>
+    ///   Allows fine grained configuration of ASP.NET Web API services.
+    /// </summary>
+    public class AspNetWebApiBuilder : IAspNetWebApiBuilder
+    {
+        /// <summary>
+        ///   Initializes a new <see cref="AspNetWebApiBuilder"/> instance.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+        public AspNetWebApiBuilder(IServiceCollection services)
+        {
+            Services = services;
+        }
+
+        /// <inheritdoc/>
+        public IServiceCollection Services { get; }
+    }
+}
