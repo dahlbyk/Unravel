b1e1d588ea1a1c84359634179cff6c009806d692
Keith Dahlby <dahlbyk@gmail.com>
2021-10-17 14:13:56 -0500

    Generate examples/Identity

---
+++ examples/Identity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnravelExamples.Identity.Startup))]
+namespace UnravelExamples.Identity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
