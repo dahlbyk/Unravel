fe957c85c253f84b6b70a09d454b789a86ee3082
Keith Dahlby <dahlbyk@gmail.com>
2021-10-24 00:50:48 -0500

    Add conflicting CreatePerOwinContext() methods
    The call is ambiguous between the following methods or properties:
    'Owin.AppBuilderExtensions.CreatePerOwinContext<T>(Owin.IAppBuilder, System.Func<T>)' and
    'Owin.UnravelIdentityAppBuilderExtensions.CreatePerOwinContext<T>(Owin.IAppBuilder, System.Func<T>)'

---
+++ src/AspNet.Identity/DependencyInjection/UnravelIdentityAppBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Unravel.AspNet.Identity.DependencyInjection;
+
+namespace Owin
+{
+    public static class UnravelIdentityAppBuilderExtensions
+    {
+        /// <summary>
+        /// "The call is ambiguous between the following methods" conflict with
+        /// <see cref="AppBuilderExtensions.CreatePerOwinContext{T}(IAppBuilder, Func{T})"/>.
+        /// Configure Identity services with <see cref="IdentityBuilder"/> instead.
+        /// </summary>
+        /// <exception cref="NotSupportedException">Do not use this method.</exception>
+        [Obsolete("Use IdentityBuilder.SetPerOwinContext<T>().")]
+        public static IAppBuilder CreatePerOwinContext<T>(this IAppBuilder app, Func<T> createCallback)
+            where T : class, IDisposable =>
+            throw new NotSupportedException("Configure Identity services with IdentityBuilder instead.");
+
+        /// <summary>
+        /// "The call is ambiguous between the following methods" conflict with
+        /// <see cref="AppBuilderExtensions.CreatePerOwinContext{T}(IAppBuilder, Func{IdentityFactoryOptions{T}, IOwinContext, T})"/>.
+        /// Configure Identity services with <see cref="IdentityBuilder"/> instead.
+        /// </summary>
+        /// <exception cref="NotSupportedException">Do not use this method.</exception>
+        [Obsolete("Use IdentityBuilder.SetPerOwinContext<T>().")]
+        public static IAppBuilder CreatePerOwinContext<T>(this IAppBuilder app, Func<IdentityFactoryOptions<T>, IOwinContext, T> createCallback)
+            where T : class, IDisposable =>
+            throw new NotSupportedException("Configure Identity services with IdentityBuilder instead.");
+
+        /// <summary>
+        /// "The call is ambiguous between the following methods" conflict with
+        /// <see cref="AppBuilderExtensions.CreatePerOwinContext{T}(IAppBuilder, Func{IdentityFactoryOptions{T}, IOwinContext, T}, Action{IdentityFactoryOptions{T}, T})"/>.
+        /// Configure Identity services with <see cref="IdentityBuilder"/> instead.
+        /// </summary>
+        /// <exception cref="NotSupportedException">Do not use this method.</exception>
+        [Obsolete("Use IdentityBuilder.SetPerOwinContext<T>().")]
+        public static IAppBuilder CreatePerOwinContext<T>(this IAppBuilder app, Func<IdentityFactoryOptions<T>, IOwinContext, T> createCallback, Action<IdentityFactoryOptions<T>, T> disposeCallback)
+            where T : class, IDisposable =>
+            throw new NotSupportedException("Configure Identity services with IdentityBuilder instead.");
+    }
+}
