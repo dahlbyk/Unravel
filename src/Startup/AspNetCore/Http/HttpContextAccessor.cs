fdcf3801e54747f4a78ad06022deac39d7f040c3
Keith Dahlby <dahlbyk@gmail.com>
2022-02-23 02:21:34 -0600

    Supersede AspNetCore IHttpContextAccessor
    System.Web doesn't work well with AsyncLocal<T>

---
+++ src/Startup/AspNetCore/Http/HttpContextAccessor.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+
+namespace Unravel.AspNetCore.Http
+{
+    /// <summary>
+    /// Stores current <see cref="Microsoft.AspNetCore.Http.HttpContext"/> in
+    /// <see cref="System.Web.HttpContext.Current"/>.Items
+    /// instead of an <see cref="AsyncLocal{T}"/> field.
+    /// </summary>
+    public class HttpContextAccessor : IHttpContextAccessor
+    {
+        /// <summary>
+        /// Key in <see cref="HttpContext.Items"/> for current <see cref="HttpContext"/>.
+        /// </summary>
+        public static readonly Type HttpContextKey = typeof(HttpContext);
+
+        public HttpContext HttpContext
+        {
+            get
+            {
+                return System.Web.HttpContext.Current?.Items[HttpContextKey] as HttpContext;
+            }
+            set
+            {
+                var httpContext = System.Web.HttpContext.Current ?? throw new InvalidOperationException("HttpContext not found.");
+                httpContext.Items[HttpContextKey] = value;
+            }
+        }
+    }
+}
