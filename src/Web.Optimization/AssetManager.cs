af44c12851978868ca7a1d03afe48cb383d70800
Keith Dahlby <dahlbyk@gmail.com>
2022-02-23 00:40:37 -0600

    wip

--- src/Web.Optimization/AssetManager.cs
+++ src/Web.Optimization/AssetManager.cs
@@ -1,13 +1,26 @@
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using WebOptimizer;
 
 namespace System.Web.Optimization
 {
     public class AssetManager
     {
+        private readonly IAssetPipeline assetPipeline;
+
+        public AssetManager(IAssetPipeline assetPipeline)
+        {
+            this.assetPipeline = assetPipeline;
+        }
+
         public virtual IHtmlString RenderExplicit(string tagFormat, string[] paths)
         {
-            var pathsToRender = DeterminePathsToRender(paths);
+#pragma warning disable CS0618 // Type or member is obsolete
+            var enableBundling = BundleTable.EnableOptimizations;
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            var pathsToRender = DeterminePathsToRender(paths, enableBundling);
 
             var sb = new StringBuilder();
 
@@ -25,8 +38,10 @@ namespace System.Web.Optimization
             return new HtmlString(Normalize(virtualPath));
         }
 
-        private IEnumerable<string> DeterminePathsToRender(IEnumerable<string> paths)
+        private IEnumerable<string> DeterminePathsToRender(IEnumerable<string> paths, bool enableBundling)
         {
+            var httpContext = HttpContext.Current.GetRequestServices().GetRequiredService<Microsoft.AspNetCore.Http.IHttpContextAccessor>()?.HttpContext;
+
             // TODO: Get from IAssetPipeline
             foreach (var path in paths)
                 yield return Normalize(path);
