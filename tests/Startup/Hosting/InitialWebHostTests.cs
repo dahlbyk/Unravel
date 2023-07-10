7c948705df53a0f471ee33471f294cd619311301
Keith Dahlby <dahlbyk@gmail.com>
2023-07-09 22:55:50 -0500

    Add tests for InitialWebHost
    Also adjust how StartAsync() throws

---
+++ tests/Startup/Hosting/InitialWebHostTests.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Unravel.Hosting;
+using Xunit;
+
+namespace Unravel.Startup.Hosting.Tests
+{
+    public class InitialWebHostTests
+    {
+        [Fact]
+        public void ServerFeaturesThrows()
+        {
+            using var host = new InitialWebHost();
+
+            Assert.Throws<InvalidOperationException>(() => host.ServerFeatures);
+        }
+
+        [Fact]
+        public void ServicesThrows()
+        {
+            using var host = new InitialWebHost();
+
+            Assert.Throws<InvalidOperationException>(() => host.Services);
+        }
+
+        [Fact]
+        public async Task StartThrows()
+        {
+            using var host = new InitialWebHost();
+
+            Assert.Throws<InvalidOperationException>(() => host.Start());
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() => host.StartAsync(default));
+        }
+
+        [Fact]
+        public async Task StopDoesNotThrow()
+        {
+            using var host = new InitialWebHost();
+
+            await host.StopAsync(default);
+        }
+    }
+}
