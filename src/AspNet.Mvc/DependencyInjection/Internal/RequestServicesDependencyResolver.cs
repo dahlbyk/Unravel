a6ff03be46e51654c0adf28e7679387b29a571b2
Keith Dahlby <dahlbyk@gmail.com>
2024-06-09 20:45:47 -0500

    Add optional fallback to dependency resolvers

--- src/AspNet.Mvc/DependencyInjection/Internal/RequestServicesDependencyResolver.cs
+++ src/AspNet.Mvc/DependencyInjection/Internal/RequestServicesDependencyResolver.cs
@@ -8,18 +8,27 @@ namespace Unravel.AspNet.Mvc.DependencyInjection.Internal
 {
     /// <summary>
     /// MVC <see cref="IDependencyResolver"/> that resolves services from
-    /// <see cref="HttpContextServicesExtensions.GetRequestServices(HttpContext?)"/>.
+    /// <see cref="HttpContextServicesExtensions.GetRequestServices(HttpContext?)"/>,
+    /// or from <see cref="RequestServicesDependencyResolver.Fallback"/> (if provided).
     /// </summary>
     public sealed class RequestServicesDependencyResolver : IDependencyResolver
     {
+        /// <summary>
+        /// An optional fallback to use for services not resolved from
+        /// <see cref="HttpContextServicesExtensions.GetRequestServices(HttpContext?)"/>.
+        /// </summary>
+        public IDependencyResolver? Fallback { get; set; }
+
         /// <inheritdoc/>
         public object? GetService(Type serviceType) =>
             HttpContext.Current.GetRequestServices()
-                .GetService(serviceType);
+                .GetService(serviceType)
+            ?? Fallback?.GetService(serviceType);
 
         /// <inheritdoc/>
         public IEnumerable<object>? GetServices(Type serviceType) =>
             HttpContext.Current.GetRequestServices()
-                .GetServices(serviceType);
+                .GetServices(serviceType)
+            ?? Fallback?.GetServices(serviceType);
     }
 }
--- src/AspNet.WebApi/DependencyInjection/Internal/RequestServicesDependencyResolver.cs
+++ src/AspNet.WebApi/DependencyInjection/Internal/RequestServicesDependencyResolver.cs
@@ -8,36 +8,54 @@ namespace Unravel.AspNet.WebApi.DependencyInjection.Internal
 {
     /// <summary>
     /// WebApi <see cref="IDependencyResolver"/> that resolves services from
-    /// <see cref="HttpContextServicesExtensions.GetRequestServices(HttpContext?)"/>.
+    /// <see cref="HttpContextServicesExtensions.GetRequestServices(HttpContext?)"/>,
+    /// or from <see cref="RequestServicesDependencyResolver.Fallback"/> (if provided).
     /// </summary>
     public sealed class RequestServicesDependencyResolver : RequestServicesDependencyScope, IDependencyResolver
     {
         /// <summary>
-        ///   Scope is managed by <see cref="Unravel.SystemWeb.ServiceScopeModule"/>.
+        /// Scope is managed by <see cref="Unravel.SystemWeb.ServiceScopeModule"/>,
+        /// but the fallback <see cref="IDependencyResolver"/> might have its own scope.
+        /// In that case we return a new <see cref="RequestServicesDependencyScope"/>.
         /// </summary>
         /// <returns>The current instance.</returns>
-        public IDependencyScope BeginScope() => this;
+        public IDependencyScope BeginScope() =>
+            Fallback is IDependencyResolver resolver
+            && resolver.BeginScope() is { } scope
+            && !ReferenceEquals(scope, this)
+                ? new RequestServicesDependencyScope { Fallback = scope }
+                : this;
     }
 
     /// <summary>
     /// WebApi <see cref="IDependencyScope"/> that resolves services from
-    /// <see cref="HttpContextServicesExtensions.GetRequestServices(HttpContext?)"/>.
+    /// <see cref="HttpContextServicesExtensions.GetRequestServices(HttpContext?)"/>,
+    /// or from <see cref="RequestServicesDependencyResolver.Fallback"/> (if provided).
     /// </summary>
     public class RequestServicesDependencyScope : IDependencyScope
     {
+        /// <summary>
+        /// An optional fallback to use for services not resolved from
+        /// <see cref="HttpContextServicesExtensions.GetRequestServices(HttpContext?)"/>.
+        /// </summary>
+        public IDependencyScope? Fallback { get; set; }
+
         /// <inheritdoc/>
         public object? GetService(Type serviceType) =>
             HttpContext.Current.GetRequestServices()
-                .GetService(serviceType);
+                .GetService(serviceType)
+            ?? Fallback?.GetService(serviceType);
 
         /// <inheritdoc/>
         public IEnumerable<object>? GetServices(Type serviceType) =>
             HttpContext.Current.GetRequestServices()
-                .GetServices(serviceType);
+                .GetServices(serviceType)
+            ?? Fallback?.GetServices(serviceType);
 
         /// <summary>
         ///   Scope is managed by <see cref="Unravel.SystemWeb.ServiceScopeModule"/>.
         /// </summary>
-        public void Dispose() { }
+        public void Dispose() =>
+            Fallback?.Dispose();
     }
 }
